9a2a7622efc885afe664d0f29bce9e9ca6943d37
Taras <t.kulias.m@gmail.com>
2021-11-02 18:56:19 +0200

    Update d19.js
    problem2

--- switch/d19.js
+++ switch/d19.js
@@ -1,7 +1,27 @@
-if (browser === 'Edge') {
-    alert('You`ve got the Edge!')
-} else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
-    alert('Okay')
-} else {
-    alert('We hope');
+// if (browser === 'Edge') {
+//     alert('You`ve got the Edge!')
+// } else if (browser === 'Chrome' 
+// || browser === 'Firefox' 
+// || browser === 'Safari' 
+// || browser === 'Opera') {
+//     alert('Okay')
+// } else {
+//     alert('We hope');
+// }
+
+const number = +prompt('Enter number 0-3', '');
+
+switch (number) {
+    case 0:
+        alert('You enter 0');
+        break;
+    case 1:
+        alert('You enter 1');
+        break;
+    case 2:
+    case 3:
+        alert('You enter 2 or 3');
+        break;
+    default:
+        alert('You enter some else');
 }
