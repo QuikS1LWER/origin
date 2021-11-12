c944f0cf1e29e90b610c2f64d8cb25ae2d763a47
Taras <t.kulias.m@gmail.com>
2021-11-12 08:51:47 +0200

    toDoList

---
+++ toDoList/ToDoList.js
@@ -0,0 +1,13 @@
+const list = {
+    "create a task": "In Progress",
+    "make a bed": "Done",
+    "write a post": "To Do",
+   }
+
+   function changeStatus("write a post", "Done")
+
+   function addTask('have a walk')
+
+   function deleteTask('have a walk')
+
+   function showList()
