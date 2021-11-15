fee83fa257ed022567bb22a2116396c3cb1ce6e5
Taras <t.kulias.m@gmail.com>
2021-11-15 16:09:46 +0200

    calc

---
+++ calculator/js/objCalc.js
@@ -0,0 +1,27 @@
+// let num1 = prompt('Give number 1','');
+// let num2 = prompt('Give number 2','');
+// let operator = prompt('Give operation','');
+// const isNotValid = (typeof num1 !== 'number' || typeof num2 !== 'number');
+    
+// if (isNotValid) {
+//     alert('Error!');
+// }
+    
+let operations = {
+    sum: num1 + num2,
+    sub: '-',
+    mult: '*',
+    dev: '/'
+}
+
+console.log(calc)
+
+
+// function calc(num1, num2) {
+    
+//     // for (key in operation) {
+//     //     console.log(num1 [key] num2);
+//     // }
+// }
+
+// calc(2, 3, operation.sum);
