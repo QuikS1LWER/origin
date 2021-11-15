f42c3beca023f7e5d990a28a9a2f7d717539ff75
Taras <t.kulias.m@gmail.com>
2021-11-15 10:43:44 +0200

    toDo

--- toDoList/ToDoList.js
+++ toDoList/ToDoList.js
@@ -1,13 +1,48 @@
 const list = {
-    "create a task": "In Progress",
-    "make a bed": "Done",
-    "write a post": "To Do",
+    'create a task': 'In Progress',
+    'make a bed': 'Done',
+    'write a post': 'To Do',
    }
 
-   function changeStatus("write a post", "Done")
+addTask(list, 'make a table');
+addTask(list, 'clean the room');
+addTask(list, 'learn JS');
+addTask(list, 'end this fucking problem');
+// deleteTask(list, 'make a bed');
+changeStatus(list, 'write a post', 'In Progress');
+changeStatus(list, 'end this fucking problem', 'In Progress');
+showList(list);
 
-   function addTask('have a walk')
+   function addTask(obj, task) {
+        obj[task] = 'To Do';
+   }
 
-   function deleteTask('have a walk')
+   function deleteTask(obj, task) {
+        delete obj[task];
+   }
 
-   function showList()
+   function changeStatus(obj, task, status) {
+        obj[task] = status;
+   }
+   function showList(obj) {
+    console.log('Todo:');   
+        for (key in obj) {
+            if (obj[key] === 'To Do') {
+            console.log(key);
+            }
+        }
+    console.log('In Progress:');
+        for (key in obj) {
+            if (obj[key] === 'In Progress') {
+                console.log(key);
+            }
+        }
+    console.log('Done:');
+        for (key in obj) {
+            if (obj[key] === 'Done') {
+                console.log(key);
+            }
+        }
+   }
+
+//    console.log(list)
