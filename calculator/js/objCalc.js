b970d604000c92a6c56f26329539b1f29ff30a7d
Taras <t.kulias.m@gmail.comqwqq>
2022-01-28 21:46:46 +0100

    object calc 3

--- calculator/js/objCalc.js
+++ calculator/js/objCalc.js
@@ -1,5 +1,12 @@
 function calc(num1, num2, operation) {
 
+    let operations = {
+        sum: num1 + num2,
+        sub: num1 - num2,
+        mult: num1 * num2,
+        div: num1 / num2,
+    }
+
     const isNotValid = (typeof num1 !== 'number' || typeof num2 !== 'number');
 
     if (isNotValid) {
@@ -7,12 +14,11 @@ function calc(num1, num2, operation) {
         return;
     }
 
-    let operations = {
-        sum: num1 + num2,
-        sub: num1 - num2,
-        mult: num1 * num2,
-        div: num1 / num2,
+    if (operations[operation]) {
+        console.log(operations[operation]);
+    } else {
+        console.log("Unknown operation!");
     }
-    console.log(operations[operation]);
 }
-calc(8, 6, 'mult')
+
+calc(8, 6, 'mu lt')
