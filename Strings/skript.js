7455c2ffdcdbfa9b8509ba8b280043c9a1f8a61c
Taras <t.kulias.m@gmail.comqwqq>
2022-02-01 20:07:48 +0100

    Marathon

---
+++ Strings/skript.js
@@ -0,0 +1,28 @@
+/*
+Напишите функцию которая преобразует полученную строку в "вертикальный вид" и выводит ее в консоль.
+    Чтобы получилось вот так:
+    showVerticalMessage('марафон')
+
+М
+а
+р
+а
+ф
+о
+н
+Если строка начинается с буквы "м" - нужно вывести эту строку с первой заглавной буквой
+Если строка больше 10 символов - вывести только первые 10*/
+
+function showVerticalMessage(str) {
+    if (str[0] = 'м') {
+        console.log(str[0].toUpperCase());
+        for (let char of str.slice(1, 10)) {
+            console.log(char);
+        }
+    } else {
+        for (let char of str.slice(0, 10)) {
+            console.log(char);
+        }
+    }
+}
+showVerticalMessage('марафонsukablyat');
