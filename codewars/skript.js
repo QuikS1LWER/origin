92933825edd1f787c16f48d4f06f97be105db942
Taras <t.kulias.m@gmail.comqwqq>
2022-02-14 22:24:27 +0100

    codewars problems

--- codewars/skript.js
+++ codewars/skript.js
@@ -5,15 +5,27 @@
 должно быть возвращено число от 0 до N.
 */
 
-
 function buildFun(n){
-
     var res = []
-
-    for (var i = 0; i< n; i++){
+    for (var i = 0; i < n; i++){
         res.push(function(){
             console.log(i)
         })
     }
-    return res
+    return res;
 }
+console.log(buildFun(5));
+
+/*Верните среднее значение данного массива,
+округленного до ближайшего целого числа.
+Массив никогда не должен быть пустым.*/
+
+/*1) посчитать сумму всех оценок, 2) вычислить среднее арифметическое, 3) округлить, 4) вернуть значение*/
+
+/*function getAverage(marks) {
+    let sum = marks.reduce((sum, current) => sum + current, 0);
+    let average = Math.floor(sum / marks.length);
+    return average;
+}
+console.log(getAverage([16, 10, 4, 14, 13, 2, 10, 12, 16, 10]))*/
+
