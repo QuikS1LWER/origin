a5d9f63ddc0377d16c5e169954ed330248d531c9
Taras <t.kulias.m@gmail.comqwqq>
2022-08-12 17:23:37 +0200

    Strada.one switch calc

---
+++ Strada.one/calc.js
@@ -0,0 +1,31 @@
+/*function calc(operator, operand1, operand2) {
+    if (operator == 'add') {
+        return (operand1 + operand2)
+    } else if (operator == 'mult') {
+        return (operand1 * operand2)
+    } else if (operator == 'subtract') {
+        return (operand1 - operand2)
+    } else {
+        return 'Error'
+    }
+}
+
+console.log(calc('add',10,5))*/
+
+function calc(operator, operand1, operand2) {
+    switch (operator) {
+        case 'add':
+            return operand1 + operand2;
+            break;
+        case 'multi':
+            return operand1 * operand2;
+            break;
+        case'subtract':
+            return operand1 - operand2;
+            break
+        default:
+            console.log('Error');
+    }
+}
+
+console.log(calc('subtract',10,5))
